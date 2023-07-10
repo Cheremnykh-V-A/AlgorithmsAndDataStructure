2ff18aed0115b0385101081db19ef83a5d825993
Vadim Cheremnykh <chere96@yandex.ru>
2023-07-10 16:33:36 +0400

    added Seminar3

--- src/main/java/Seminars/Seminar3/Main.java
+++ src/main/java/Seminars/Seminar3/Main.java
@@ -1,2 +1,18 @@
-package Seminars.Seminar3;public class Main {
+package Seminars.Seminar3;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(5);
+        list.add(2);
+        list.add(8);
+        list.add(11);
+        list.add(1);
+
+        list.print();
+        list.quickSort();
+        list.print();
+
+    }
 }
